16f89ef0b69b3e597bdcaa77306e2243b1cfbfc7
abrahamjo25 <abirham.johannes@gmail.com>
2024-06-07 17:03:04 +0300

    creating Top Cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./_components/Header";
 import Sidbar from "./_components/Sidbar";
+import TopCards from "./_components/TopCards";
 export default function Home() {
   return (
     <main className="min-h-screen text-black bg-gray-100">
       <Sidbar/>
       <Header />
+      <TopCards/>
     </main>
   );
 }
