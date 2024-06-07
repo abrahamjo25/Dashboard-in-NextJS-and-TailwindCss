422bf4dadb5ccce177a13bd1e381fdec41f4547e
abrahamjo25 <abirham.johannes@gmail.com>
2024-06-07 16:45:52 +0300

    Creating Side bar component

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between px-4 ml-24 pt-2">
+      <h1 className="text-xl">Dashboard</h1>
+      <h2> Welcomback Client</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/_components/Sidbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { RxDashboard, RxPerson, RxSketchLogo } from "react-icons/rx";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { IoSettingsOutline } from "react-icons/io5";
+const Sidbar = () => {
+  return (
+    <div className="flex">
+      <div className="fixed w-20  shadow-md h-screen flex flex-col p-4 bg-white justify-between items-end">
+        <div className="flex flex-col items-center">
+          <div className="bg-purple-800 rounded-lg p-3 inline-block text-white">
+            <RxSketchLogo size={20} />
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer rounded-lg p-3 inline-block my-6">
+            <RxDashboard size={20} />
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer rounded-lg p-3 inline-block my-4">
+            <RxPerson size={20} />
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer rounded-lg p-3 inline-block my-4">
+            <HiOutlineShoppingBag size={20} />
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer rounded-lg p-3 inline-block my-4">
+            <IoSettingsOutline size={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidbar;
